5a3433176426a6a5e9f01a425ef53528bc6106ca
hudsonsistemasp <hudsonsistemasp@gmail.com>
2020-01-22 14:17:19 -0300

    Criação dos métodos POST e DELETE.

--- src/main/java/com/mongdb/mongodbTest/resource/UserResource.java
+++ src/main/java/com/mongdb/mongodbTest/resource/UserResource.java
@@ -1,14 +1,17 @@
 package com.mongdb.mongodbTest.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mongdb.mongodbTest.domain.User;
 import com.mongdb.mongodbTest.dto.UserDTO;
@@ -31,11 +34,25 @@ public class UserResource {
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = userService.findById(id);
 		UserDTO userDTO = new UserDTO(user);
 		return ResponseEntity.ok().body(userDTO);
 	}
-
+	
+	//@PostMapping - funciona como abaixo
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insertUser(@RequestBody UserDTO userDTO){
+		User objUser = userService.fromDTO(userDTO);
+		User obj = userService.insert(objUser);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//Traz todo o cabeçalho contendo a localização do novo recurso criado
+		return ResponseEntity.created(uri).build();//created retorna o codigo 201
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		//cod. 204 = Quando se faz uma operação e não precisa trazer uma resposta
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/mongdb/mongodbTest/service/UserService.java
+++ src/main/java/com/mongdb/mongodbTest/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongdb.mongodbTest.domain.User;
+import com.mongdb.mongodbTest.dto.UserDTO;
 import com.mongdb.mongodbTest.repository.UserRepository;
 import com.mongdb.mongodbTest.service.exception.ObjectNotFoundException;
 
@@ -23,6 +24,24 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> objUser = userRepository.findById(id);
 		return objUser.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id); //Usar o método acima, pois assim se não encontrar o id, já gera a exceção pra gente
+		userRepository.deleteById(id);
+	}
+	
+	
+	/*Fez a implementação do DTO aqui, por que essa classe já acesso banco de dados direto e fica mais estratégico usar na Resource.
+	 Mas é correto desenhar na classe DTO também, tudo vai da estratégia momentâena.*/
+	
+	public User fromDTO(UserDTO objUserDTO) {
+		System.out.println(objUserDTO.getId() + " " + objUserDTO.getName() + " " + objUserDTO.getEmail());
+		return new User(objUserDTO.getId(),objUserDTO.getName(),objUserDTO.getEmail(),"password Padrao");
 	}
 	
 }
